f98dfdba3196288e70e7976c2c3881ae8e166d90
lening rodas <lening.a.rodas@gmail.com>
2021-01-19 14:43:11 -0600

    changed it with walkthrough over my answer

--- src/main/java/name.java
+++ src/main/java/name.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-@WebServlet(name = "nameServlet", urlPatterns = "/count")
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
 public class name extends HttpServlet {
     private int counter = 0;
 
@@ -19,21 +19,30 @@ public class name extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
 
-        String name = req.getParameter("name");
-        counter +=1;
+        String reset = req.getParameter("reset");
+
         out.println(counter);
-        if(name == null){
+
+
+            if(reset != null){
+                counter = 0;
+                out.println("page has been viewed " + counter + " of times");
+            }
             counter += 1;
-            out.println(counter);
-        } else if (name.equals("reset")){
-            counter = 0;
-            out.println(counter);
-        } else {
-            counter +=1;
-            out.println(counter);
-        }
 
+            out.println("<h1>page has been viewed " + counter + " of times </h1>");
 
+//
+//        if(reset == null){
+//            counter += 1;
+//            out.println(counter);
+//        } else if (reset.equals("reset")){
+//            counter = 0;
+//            out.println(counter);
+//        } else {
+//            counter +=1;
+//            out.println(counter);
+//        }
 
     }
 }
